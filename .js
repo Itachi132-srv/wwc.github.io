76466e8b622e398093edc3a19eadcedfd5c4b5de
Itachi132-srv <hanzalaazhar143@gmail.com>
2026-03-25 04:14:41 +0500

    Update .js

--- .js
+++ .js
@@ -1,15 +1,16 @@
-// --- Simplified Model Loading (Sab kuch ek hi jagah) ---
-const mtlLoader = new MTLLoader();
-mtlLoader.load('car1.mtl', (materials) => {
-    materials.preload();
-    
-    const objLoader = new OBJLoader();
-    objLoader.setMaterials(materials);
-    objLoader.load('car1.obj', (object) => {
-        player = object;
-        player.scale.set(0.1, 0.1, 0.1); 
-        player.position.y = 0;
-        scene.add(player);
-    });
-});
+// Is line ko road loading ke andar replace karein
+const roadTex = new THREE.TextureLoader().load('road.png');
 
+loadModel('road.mtl', 'road.obj', (obj) => {
+    obj.traverse((child) => {
+        if (child.isMesh) {
+            child.material.map = roadTex; // Direct road.png apply karna
+        }
+    });
+    for(let i = 0; i < 5; i++) {
+        let r = obj.clone();
+        r.position.z = -i * 60;
+        scene.add(r);
+        roads.push(r);
+    }
+});
